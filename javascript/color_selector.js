3033ecf62309156133844bfb95600dccce2399f8
Josh Choi <joshuachoi0508@gmail.com>
2019-04-15 12:40:20 -0700

    Color saves, counter raises

--- javascript/color_selector.js
+++ javascript/color_selector.js
@@ -1,9 +1,17 @@
+const counter = document.getElementById("counter");
+const ball = document.getElementById("ball");
+
 if (!document.cookie) {
   const colors = ["red", "blue"]
   const color = colors[Math.floor(Math.random() * 2)];
-  
-  const ball = document.getElementById("ball");
-  ball.style.background = color;
 
-  document.cookie = `${color};1`;
+  ball.style.background = color;
+  document.cookie = color + ";1";
+  counter.innerText = 1;
+} else {
+  const colorAndCount = document.cookie.split(";");
+
+  ball.style.background = colorAndCount[0];
+  document.cookie = colorAndCount[0] + colorAndCount[1] + 1;
+  counter.innerText = colorAndCount[1] + 1;
 }
