08d8ddb770be8c407fd5ea31b83cfb78a068765f
Josh Choi <joshuachoi0508@gmail.com>
2019-04-15 15:03:51 -0700

    Insert description comments

--- javascript/color_selector.js
+++ javascript/color_selector.js
@@ -1,15 +1,18 @@
+//raises color count and uses it to set 
+//the new cookie and add new innerText for counter
 const raiseColorCount = () => {
   //splits cookie into an array [color, count]
   const colorAndCount = document.cookie.split(",");
   const color = colorAndCount[0];
-  const count = colorAndCount[1];
-  const nextCount = Number(count) + 1;
+  const previousCount = colorAndCount[1];
+  const nextCount = Number(previousCount) + 1;
 
   ball.style.background = `linear-gradient(${color}, dark${color})`;
   document.cookie = color + "," + String(nextCount);
   counter.innerText = "Count: " + String(nextCount);
 }
 
+//picks a random color and colors the circle with the chosen color
 const pickRandomColor = () => {
   const color = colors[Math.floor(Math.random() * 2)];
 
@@ -18,6 +21,8 @@ const pickRandomColor = () => {
   counter.innerText = "Count: 1";
 }
 
+//decides whether to pick a random color or raise the color count depending on
+//the existence of cookie
 if (!document.cookie) {
   pickRandomColor();
 } else {
--- javascript/reset_cookie.js
+++ javascript/reset_cookie.js
@@ -1,3 +1,6 @@
+//adds event listener to the reset button to reset cookie and 
+//choose a random color again
 resetButton.addEventListener("click", () => {
+  //pickRandomColor is in color_selector.js
   pickRandomColor();
 })
