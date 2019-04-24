e2f1dc333891ba8e941145b075d0db1b21f192d7
Josh Choi <joshuachoi0508@gmail.com>
2019-04-24 14:56:52 -0700

    Reassign reset button's event listener callback function

--- javascript/reset_cookie.js
+++ javascript/reset_cookie.js
@@ -1,6 +1,3 @@
 //adds event listener to the reset button to reset cookie and 
 //choose a random color again
-resetButton.addEventListener("click", () => {
-  //pickRandomColor is in color_selector.js
-  pickRandomColor();
-})
+resetButton.addEventListener("click", pickRandomColor)
